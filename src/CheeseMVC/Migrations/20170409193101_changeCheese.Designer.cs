987901eb75d83715494d663a0bfb4e717ef84bcb
Mark Reichert <mark.a.reichert1@gmail.com>
2017-04-10 18:42:03 -0500

    Commit of Part 2

--- src/CheeseMVC/Migrations/20170409193101_changeCheese.Designer.cs
+++ src/CheeseMVC/Migrations/20170409193101_changeCheese.Designer.cs
@@ -8,9 +8,10 @@ using CheeseMVC.Data;
 namespace CheeseMVC.Migrations
 {
     [DbContext(typeof(CheeseDbContext))]
-    partial class CheeseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170409193101_changeCheese")]
+    partial class changeCheese
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
